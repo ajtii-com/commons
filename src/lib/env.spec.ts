c6617f847dd0296ec9845f77ea5d2363702fe069
David Bojnansky <david.bojnansky@gmail.com>
2018-03-13 18:58:48 +0100

    20180313T1858

--- src/lib/env.spec.ts
+++ src/lib/env.spec.ts
@@ -6,11 +6,14 @@ function forgetModule() {
   delete require.cache[require.resolve('./env')];
 }
 
-function requireModule(env: string) {
+function requireModule(env: string, srvEnv = 'development') {
+  let oldSrvEnv = process.env.SRV_ENV;
   let oldEnv = process.env.NODE_ENV;
+  process.env.SRV_ENV = srvEnv;
   process.env.NODE_ENV = env;
   forgetModule();
   let m = require('./env');
+  process.env.SRV_ENV = oldSrvEnv;
   process.env.NODE_ENV = oldEnv;
   return m;
 }
@@ -19,88 +22,178 @@ let c: () => void;
 
 let e: typeof env;
 
-describe('when NODE_ENV is not set', () => {
-  before(() => {
-    c = () => requireModule('');
+describe('SRV_ENV and related constants', () => {
+  describe('when SRV_ENV is not set', () => {
+    before(() => {
+      e = requireModule('development', '');
+    });
+
+    it('should set SRV_ENV to development', () => {
+      expect(e.SRV_ENV).to.eq('development');
+    });
   });
 
-  it('should throw an error', () => {
-    expect(c).to.throw(AjtiiError, 'not set');
+  describe('when SRV_ENV is set to invalid value', () => {
+    before(() => {
+      c = () => requireModule('development', 'wrong');
+    });
+
+    it('should throw an error', () => {
+      expect(c).to.throw(TypeError, /^SRV_ENV.+one of these/);
+    });
+  });
+
+  describe('when SRV_ENV = development', () => {
+    before(() => {
+      e = requireModule('development', 'development');
+    });
+
+    it('should IS_DEV_SRV = true', () => {
+      expect(e.IS_DEV_SRV).to.eq(true);
+    });
+
+    it('should IS_DEV_BUT_NOT_TEST_SRV = true', () => {
+      expect(e.IS_DEV_BUT_NOT_TEST_SRV).to.eq(true);
+    });
+
+    it('should IS_TEST_SRV = false', () => {
+      expect(e.IS_TEST_SRV).to.eq(false);
+    });
+
+    it('should IS_PROD_SRV = false', () => {
+      expect(e.IS_PROD_SRV).to.eq(false);
+    });
+  });
+
+  describe('when SRV_ENV = testing', () => {
+    before(() => {
+      e = requireModule('development', 'testing');
+    });
+
+    it('should IS_DEV_SRV = true', () => {
+      expect(e.IS_DEV_SRV).to.eq(true);
+    });
+
+    it('should IS_DEV_BUT_NOT_TEST_SRV = false', () => {
+      expect(e.IS_DEV_BUT_NOT_TEST_SRV).to.eq(false);
+    });
+
+    it('should IS_TEST_SRV = true', () => {
+      expect(e.IS_TEST_SRV).to.eq(true);
+    });
+
+    it('should IS_PROD_SRV = false', () => {
+      expect(e.IS_PROD_SRV).to.eq(false);
+    });
+  });
+
+  describe('when SRV_ENV = production', () => {
+    before(() => {
+      e = requireModule('development', 'production');
+    });
+
+    it('should IS_DEV_SRV = false', () => {
+      expect(e.IS_DEV_SRV).to.eq(false);
+    });
+
+    it('should IS_DEV_BUT_NOT_TEST_SRV = false', () => {
+      expect(e.IS_DEV_BUT_NOT_TEST_SRV).to.eq(false);
+    });
+
+    it('should IS_TEST_SRV = false', () => {
+      expect(e.IS_TEST_SRV).to.eq(false);
+    });
+
+    it('should IS_PROD_SRV = true', () => {
+      expect(e.IS_PROD_SRV).to.eq(true);
+    });
   });
 });
 
-describe('when NODE_ENV is set to invalid value', () => {
-  before(() => {
-    c = () => requireModule('wrong');
+describe('ENV and related constants', () => {
+  describe('when NODE_ENV is not set', () => {
+    before(() => {
+      c = () => requireModule('');
+    });
+
+    it('should throw an error', () => {
+      expect(c).to.throw(TypeError, 'not set');
+    });
   });
 
-  it('should throw an error', () => {
-    expect(c).to.throw(AjtiiError, 'one of these');
-  });
-});
+  describe('when NODE_ENV is set to invalid value', () => {
+    before(() => {
+      c = () => requireModule('wrong');
+    });
 
-describe('when NODE_ENV = development', () => {
-  before(() => {
-    e = requireModule('development');
-  });
-
-  it('should IN_DEV = true', () => {
-    expect(e.IN_DEV).to.eq(true);
+    it('should throw an error', () => {
+      expect(c).to.throw(TypeError, /^NODE_ENV.+one of these/);
+    });
   });
 
-  it('should IN_DEV_BUT_NOT_TEST = true', () => {
-    expect(e.IN_DEV_BUT_NOT_TEST).to.eq(true);
+  describe('when NODE_ENV = development', () => {
+    before(() => {
+      e = requireModule('development');
+    });
+
+    it('should IN_DEV = true', () => {
+      expect(e.IN_DEV).to.eq(true);
+    });
+
+    it('should IN_DEV_BUT_NOT_TEST = true', () => {
+      expect(e.IN_DEV_BUT_NOT_TEST).to.eq(true);
+    });
+
+    it('should IN_TEST = false', () => {
+      expect(e.IN_TEST).to.eq(false);
+    });
+
+    it('should IN_PROD = false', () => {
+      expect(e.IN_PROD).to.eq(false);
+    });
   });
 
-  it('should IN_TEST = false', () => {
-    expect(e.IN_TEST).to.eq(false);
+  describe('when NODE_ENV = testing', () => {
+    before(() => {
+      e = requireModule('testing');
+    });
+
+    it('should IN_DEV = true', () => {
+      expect(e.IN_DEV).to.eq(true);
+    });
+
+    it('should IN_DEV_BUT_NOT_TEST = false', () => {
+      expect(e.IN_DEV_BUT_NOT_TEST).to.eq(false);
+    });
+
+    it('should IN_TEST = true', () => {
+      expect(e.IN_TEST).to.eq(true);
+    });
+
+    it('should IN_PROD = false', () => {
+      expect(e.IN_PROD).to.eq(false);
+    });
   });
 
-  it('should IN_PROD = false', () => {
-    expect(e.IN_PROD).to.eq(false);
-  });
-});
+  describe('when NODE_ENV = production', () => {
+    before(() => {
+      e = requireModule('production');
+    });
 
-describe('when NODE_ENV = testing', () => {
-  before(() => {
-    e = requireModule('testing');
-  });
+    it('should IN_DEV = false', () => {
+      expect(e.IN_DEV).to.eq(false);
+    });
 
-  it('should IN_DEV = true', () => {
-    expect(e.IN_DEV).to.eq(true);
-  });
+    it('should IN_DEV_BUT_NOT_TEST = false', () => {
+      expect(e.IN_DEV_BUT_NOT_TEST).to.eq(false);
+    });
 
-  it('should IN_DEV_BUT_NOT_TEST = false', () => {
-    expect(e.IN_DEV_BUT_NOT_TEST).to.eq(false);
-  });
+    it('should IN_TEST = false', () => {
+      expect(e.IN_TEST).to.eq(false);
+    });
 
-  it('should IN_TEST = true', () => {
-    expect(e.IN_TEST).to.eq(true);
-  });
-
-  it('should IN_PROD = false', () => {
-    expect(e.IN_PROD).to.eq(false);
-  });
-});
-
-describe('when NODE_ENV = production', () => {
-  before(() => {
-    e = requireModule('production');
-  });
-
-  it('should IN_DEV = false', () => {
-    expect(e.IN_DEV).to.eq(false);
-  });
-
-  it('should IN_DEV_BUT_NOT_TEST = false', () => {
-    expect(e.IN_DEV_BUT_NOT_TEST).to.eq(false);
-  });
-
-  it('should IN_TEST = false', () => {
-    expect(e.IN_TEST).to.eq(false);
-  });
-
-  it('should IN_PROD = true', () => {
-    expect(e.IN_PROD).to.eq(true);
+    it('should IN_PROD = true', () => {
+      expect(e.IN_PROD).to.eq(true);
+    });
   });
 });
--- src/lib/env.ts
+++ src/lib/env.ts
@@ -3,16 +3,55 @@ import { AjtiiError } from './error';
 export type Env = 'development' | 'testing' | 'production';
 
 /**
+ * SRV_ENV
+ *
+ * Default: development
+ */
+export const SRV_ENV: Env = process.env.SRV_ENV as Env || 'development';
+
+if (SRV_ENV !== 'development' && SRV_ENV !== 'testing'
+    && SRV_ENV !== 'production')
+  throw new TypeError(`SRV_ENV must be one of these: development, testing
+      or production; ${SRV_ENV} given`);
+
+console.info(`SRV_ENV = ${SRV_ENV}`);
+
+/**
+ * SRV_ENV = testing
+ */
+export const IS_TEST_SRV = SRV_ENV === 'testing';
+
+/**
+ * SRV_ENV = development
+ */
+export const IS_DEV_BUT_NOT_TEST_SRV = SRV_ENV === 'development';
+
+/**
+ * SRV_ENV = development or testing
+ */
+export const IS_DEV_SRV = IS_DEV_BUT_NOT_TEST_SRV || IS_TEST_SRV;
+
+/**
+ * SRV_ENV = production
+ */
+export const IS_PROD_SRV = SRV_ENV === 'production';
+
+/**
  * NODE_ENV
  */
 export const ENV = process.env.NODE_ENV as Env;
 
+// Reason why an error is thrown when NODE_ENV is not set at all is
+// that the other modules can use/depend on NODE_ENV too because
+// it is well-known environment variable
 if (!ENV)
-  throw new AjtiiError("NODE_ENV is not set");
+  throw new TypeError("NODE_ENV is not set");
 
 if (ENV !== 'development' && ENV !== 'testing' && ENV !== 'production')
-  throw new AjtiiError(`NODE_ENV must be one of these: development, testing
-      or production; %s given`, ENV);
+  throw new TypeError(`NODE_ENV must be one of these: development, testing
+      or production; ${ENV} given`);
+
+console.info(`ENV = ${ENV}`);
 
 /**
  * ENV = testing
